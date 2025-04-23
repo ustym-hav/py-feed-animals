6782c0c91ecf6b00e47e437fde72d2726c0775d4
Ustym <Ustym.hav@gmail.com>
2025-04-23 20:37:57 +0300

    animal_master

--- app/main.py
+++ app/main.py
@@ -1,1 +1,56 @@
-#  write your code here
+from typing import List
+
+
+class Animal:
+
+    def __init__(
+        self, name: str,
+        appetite: int,
+        is_hungry: bool = True
+    ) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def print_name(self) -> None:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self) -> int:
+        if self.is_hungry is not True:
+            return 0
+        print(f"Eating {self.appetite} food points...")
+        self.is_hungry = False
+        return self.appetite
+
+
+class Cat(Animal):
+    def __init__(
+        self, name: str,
+        appetite: int = 3,
+        is_hungry: bool = True
+    ) -> None:
+        super().__init__(name, appetite, is_hungry)
+
+    def catch_mouse(self) -> None:
+        print("The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(
+        self, name:
+        str, appetite:
+        int = 7, is_hungry:
+        bool = True
+    ) -> None:
+        super().__init__(name, appetite, is_hungry)
+
+    def bring_slippers(self) -> None:
+        print("The slippers delivered!")
+
+
+def feed_animals(animals: List[Animal]) -> int:
+    sum_of_food_points = 0
+    for animal in animals:
+        food = animal.feed()
+        sum_of_food_points += food
+    return sum_of_food_points
